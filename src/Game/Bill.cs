145f10509dd5455539fa61f9590f10a169f6ae4c
carlos1798 <carlosepila98@gmail.com>
2023-12-09 16:57:07 +0100

    10bill

---
+++ src/Game/Bill.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace monoos.src.Game
+{
+    internal class Bill
+    {
+        private int id;
+
+        public enum BillType
+        {
+            One = 1,
+            Five = 5,
+            Ten = 10,
+        }
+    }
+}
